3b160ced9c76dce89b11174acfe1626842cf87f4
shuangxuetwelve <shuangxue12@outlook.com>
2021-11-26 16:22:24 -0500

    Add a text reflection

--- lib/components/input.dart
+++ lib/components/input.dart
@@ -1,19 +1,37 @@
 import 'package:flutter/material.dart';
 
 class Input extends StatefulWidget {
+  String _placeholder = "";
+  void Function(String)? _onChanged;
+
+  Input({String placeholder = "", void Function(String)? onChanged}) {
+    _placeholder = placeholder;
+    _onChanged = onChanged;
+  }
+
   @override
-  _InputState createState() => _InputState();
+  _InputState createState() =>
+      _InputState(placeholder: _placeholder, onChanged: _onChanged);
 }
 
 class _InputState extends State<Input> {
+  String _placeholder = "";
+  void Function(String)? _onChanged;
+
+  _InputState({String placeholder = "", void Function(String)? onChanged}) {
+    _placeholder = placeholder;
+    _onChanged = onChanged;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: const TextField(
+      child: TextField(
         decoration: InputDecoration(
-          border: OutlineInputBorder(),
-          labelText: "Text",
+          border: const OutlineInputBorder(),
+          labelText: _placeholder,
         ),
+        onChanged: _onChanged,
       ),
       margin: const EdgeInsets.symmetric(vertical: 20),
     );
--- lib/pages/input.dart
+++ lib/pages/input.dart
@@ -1,15 +1,39 @@
 import 'package:flutter/material.dart';
 import '../components/input.dart';
 
-class InputPage extends StatelessWidget {
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  String _text = "";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Input Page Title")),
       body: SizedBox.expand(
-        child: Column(
-          children: <Widget>[SizedBox(width: 300, child: Input())],
-        ),
+        child: Column(children: <Widget>[
+          Column(
+            children: <Widget>[
+              SizedBox(
+                  width: 300,
+                  child: Input(
+                    placeholder: "Placeholder",
+                    onChanged: (String text) {
+                      setState(() {
+                        _text = text;
+                      });
+                    },
+                  )),
+              Container(
+                  child: Text("Text: $_text"),
+                  margin: const EdgeInsets.symmetric(horizontal: 13)),
+            ],
+            crossAxisAlignment: CrossAxisAlignment.start,
+          )
+        ]),
       ),
     );
   }
