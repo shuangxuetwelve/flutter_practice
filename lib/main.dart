dec234b288a81ca8db9afc82f7e6819ca60dc04b
shuangxuetwelve <shuangxue12@outlook.com>
2021-11-26 03:34:24 -0500

    Initialize repo

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(App());
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Flutter Demo",
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Welcome to Flutter"),
+      ),
+      body: Center(
+        child: GestureDetector(
+          child: const Text("Input Page"),
+          onTap: () {
+            Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+              return Scaffold(
+                appBar: AppBar(
+                  title: const Text("Input Page Title"),
+                ),
+                body: const Center(
+                  child: Text("Input Page Content"),
+                ),
+              );
+            }));
+          },
+        ),
+      ),
+    );
+  }
+}
