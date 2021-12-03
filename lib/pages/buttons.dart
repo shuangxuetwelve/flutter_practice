6abe06214517c7213213e6accc802a69ae79a571
shuangxuetwelve <shuangxue12@outlook.com>
2021-12-03 17:45:55 -0500

    Add the floating button on page buttons

--- lib/pages/buttons.dart
+++ lib/pages/buttons.dart
@@ -5,23 +5,29 @@ import 'package:flutter/material.dart';
 class ButtonsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SizedBox.expand(
-      child: Column(
-        children: <Widget>[
-          Container(
-            child: SizedBox(
-              child: ElevatedButton(
-                child: const Text(
-                  "ElevatedButton",
+    return Scaffold(
+      body: SizedBox.expand(
+        child: Column(
+          children: <Widget>[
+            Container(
+              child: SizedBox(
+                child: ElevatedButton(
+                  child: const Text(
+                    "ElevatedButton",
+                  ),
+                  onPressed: () {},
                 ),
-                onPressed: () {},
+                width: 300,
+                height: 50,
               ),
-              width: 300,
-              height: 50,
+              margin: const EdgeInsets.symmetric(vertical: 20),
             ),
-            margin: const EdgeInsets.symmetric(vertical: 20),
-          ),
-        ],
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
       ),
     );
   }
