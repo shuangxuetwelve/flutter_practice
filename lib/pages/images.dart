168431339e73a9db35ee8f35b05707e7c8d78aaf
luqiuyuan <77678255+shuangxuetwelve@users.noreply.github.com>
2021-12-10 16:55:11 -0500

    Working on page images

--- lib/pages/images.dart
+++ lib/pages/images.dart
@@ -5,22 +5,98 @@ class Page extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox.expand(
-      child: Column(
-        children: <Widget>[
-          const Text("Asset image"),
-          Image.asset(
+    return ListView(
+      children: <Widget>[
+        const Center(
+          child: Text("Asset image"),
+        ),
+        Center(
+          child: Image.asset(
             "assets/images/colors.webp",
             width: 300,
+            height: 300,
           ),
-          const Text("Network image"),
-          Image.network(
+        ),
+        const Center(
+          child: Text("Network image"),
+        ),
+        Center(
+          child: Image.network(
             "https://images.unsplash.com/photo-1639074916237-5d24bc87e0d5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=988&q=80",
             width: 300,
             height: 300,
           ),
-        ],
-      ),
+        ),
+        const Center(
+          child: Text("Fit pattern: contain"),
+        ),
+        Center(
+          child: Container(
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Theme.of(context).colorScheme.primary,
+                width: 2,
+              ),
+            ),
+            child: Image.asset(
+              "assets/images/starbucks.webp",
+              width: 300,
+              height: 300,
+              fit: BoxFit.contain,
+            ),
+          ),
+        ),
+        const Center(
+          child: Text("Fit pattern: cover"),
+        ),
+        Center(
+          child: Container(
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Theme.of(context).colorScheme.primary,
+                width: 2,
+              ),
+            ),
+            child: Image.asset(
+              "assets/images/starbucks.webp",
+              width: 300,
+              height: 300,
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+        const Center(
+          child: Text("Fit pattern: fill"),
+        ),
+        Center(
+          child: Container(
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Theme.of(context).colorScheme.primary,
+                width: 2,
+              ),
+            ),
+            child: Image.asset(
+              "assets/images/starbucks.webp",
+              width: 300,
+              height: 300,
+              fit: BoxFit.fill,
+            ),
+          ),
+        ),
+        const Center(
+          child: Text("Fit pattern: fill"),
+        ),
+        Center(
+          child: Image.asset(
+            "assets/images/starbucks.webp",
+            width: 300,
+            height: 300,
+            fit: BoxFit.cover,
+            alignment: Alignment.topRight,
+          ),
+        ),
+      ],
     );
   }
 }
