70ab7f69140b4c5e1b20c94653c3de8278c6f761
luqiuyuan <77678255+shuangxuetwelve@users.noreply.github.com>
2021-12-10 14:38:47 -0500

    Working on page images

--- lib/pages/images.dart
+++ lib/pages/images.dart
@@ -5,6 +5,22 @@ class Page extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox.expand();
+    return SizedBox.expand(
+      child: Column(
+        children: <Widget>[
+          const Text("Asset image"),
+          Image.asset(
+            "assets/images/colors.webp",
+            width: 300,
+          ),
+          const Text("Network image"),
+          Image.network(
+            "https://images.unsplash.com/photo-1639074916237-5d24bc87e0d5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=988&q=80",
+            width: 300,
+            height: 300,
+          ),
+        ],
+      ),
+    );
   }
 }
